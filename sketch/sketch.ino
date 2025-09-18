37c23e707f02447df39f2eadebabd486fd0f2026
Jagadishpediredla <149489074+Jagadishpediredla@users.noreply.github.com>
2025-09-18 11:33:25 +0530

    Compile firmware for arduino:avr:uno

---
+++ sketch/sketch.ino
@@ -0,0 +1,16 @@
+/*
+  Led blink
+*/
+
+const int ledPin = 13; // Define the LED pin
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // Set the LED pin as an output
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // Turn the LED on
+  delay(1000); // Wait for 1 second
+  digitalWrite(ledPin, LOW); // Turn the LED off
+  delay(1000); // Wait for 1 second
+}
